8d389386e0526d58d9040da7c63407ece65b41e5
SujanTuladhar-Outcode <sujan.tuladhar@outcodesoftware.com>
2023-06-05 21:30:35 +0545

    master [New]: Added Login and Dashboard

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = "core"
 
 urlpatterns = [
-
+    path('', views.login_view, name='login'),
+    path('login/', views.login_view, name='user_login'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('core:dashboard')
+        else:
+            error_message = 'Invalid email or password'
+    else:
+        error_message = None
+
+    return render(request, 'user/login.html', {'error_message': error_message})
+
+
+@login_required
+def dashboard(request):
+    return render(request, 'dashboard.html')
